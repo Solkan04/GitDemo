db8cb9b0c47c8ff8694d887bfc61c8e6b88f3e61
Solkan04 <58631405+Solkan04@users.noreply.github.com>
2020-08-29 22:39:10 +0530

    Update TestUtils.java

--- src/main/java/com/home/Utility/TestUtils.java
+++ src/main/java/com/home/Utility/TestUtils.java
@@ -9,5 +9,10 @@ public class TestUtils
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        method1();
+    }
+    public static void method1()
+    {
+        System.out.println( "Hello " );
     }
 }
