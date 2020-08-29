0e8832a721a6ef0b48edeaf28e1e78dd63549632
Solkan04 <58631405+Solkan04@users.noreply.github.com>
2020-08-29 23:23:49 +0530

    Update TC_001.java

--- src/test/java/com/home/TestScripts/TC_001.java
+++ src/test/java/com/home/TestScripts/TC_001.java
@@ -2,6 +2,11 @@ package com.home.TestScripts;
 
 public class TC_001 {
 
+		public static void main() 
+	{
+		// TODO Auto-generated method stub
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
