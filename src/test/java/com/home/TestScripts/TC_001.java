f53d018a790f5128053e5fa78ba696cc4d6f53fd
Mahesh <solkanmahesh@gmail.com>
2020-08-29 23:26:20 +0530

    Added New Method in TC_001.class

--- src/test/java/com/home/TestScripts/TC_001.java
+++ src/test/java/com/home/TestScripts/TC_001.java
@@ -1,10 +1,13 @@
 package com.home.TestScripts;
 
-public class TC_001 {
-
-	public static void main(String[] args) {
+public class TC_001 
+{
+	public static void main() 
+	{
 		// TODO Auto-generated method stub
-
 	}
-
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+	}
 }
