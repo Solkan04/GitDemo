3383df2cce0fbfceb615310ff803f2c74f86a384
Solkan <solkanmahesh@gmail.com>
2020-11-23 00:21:50 +0530

    committing SOLK-123

---
+++ src/main/java/com/home/Utility/TC001.java
@@ -0,0 +1,11 @@
+package com.home.Utility;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class TC001
+{
+    public static void main()
+    {
+        System.out.println();
+    }
+}
