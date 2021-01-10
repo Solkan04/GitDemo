7dbf9ba528e1072d596262fce1be025bdd143471
Solkan <solkanmahesh@gmail.com>
2021-01-10 22:39:54 +0530

    Committing Mahesh

--- src/test/java/com/home/TestScripts/WorkingStash.java
+++ src/test/java/com/home/TestScripts/WorkingStash.java
@@ -2,22 +2,8 @@ package com.home.TestScripts;
 
 public class WorkingStash {
 
-    //jdhfbhjb
-    //ldfbhkljhvb
-    //ljhdgbvsajbv
-
-    //4563532573
-    //4532453
-    //453453
-
-<<<<<<< HEAD
-    public static void message()
+    public static void min(String[] args)
     {
-
-=======
-    public void getOut()
-    {
-        
->>>>>>> 6b0b3ef... Committing Mahesh
+        System.out.println("Mahesh");
     }
 }
